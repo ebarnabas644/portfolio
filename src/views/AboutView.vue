dcf422976c9bddab57bd35550e1f3a78c477b06d
ebarnabas <macskabal2000@gmail.com>
2023-06-13 22:28:27 +0200

    Removing unused view

--- src/views/AboutView.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <main class="flex">
-    <div class="about">
-    <h1 class="bg-slate-500">This is an about page</h1>
-    <p class="text-cyan-500">asd</p>
-  </div>
-  </main>
-</template>
