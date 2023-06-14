c74a1510579f30ed766b10b1d2bf7814674a9b9c
ebarnabas <macskabal2000@gmail.com>
2023-06-14 22:02:14 +0200

    Layout changes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,18 +9,6 @@ import Card from '@/components/Card.vue';
       <h2 class="text-2xl">Junior szoftver fejlesztő</h2>
     </div>
     <div class="p-5 max-w-xl">
-      <Card>
-        Card lul<br>
-        Card lul<br>
-        Card lul<br>
-        Card lul<br>
-      </Card>
-      <Card>
-        Card lul<br>
-        Card lul<br>
-        Card lul<br>
-        Card lul<br>
-      </Card>
       <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vulputate lacinia tortor congue volutpat. Sed nec magna consequat, tincidunt mauris at, faucibus eros. Nam tincidunt risus tortor, vitae semper urna accumsan vitae. In magna nisl, accumsan non tincidunt non, vestibulum sit amet mi. Quisque euismod quam in metus varius, vel lacinia sapien blandit. Aenean facilisis augue eget nisl pellentesque, et vestibulum lacus volutpat. Maecenas laoreet diam leo, sed elementum eros faucibus at. Nunc egestas varius nisi nec ultricies. Nulla ut varius nibh, vestibulum pellentesque nunc. Duis at pellentesque nunc. Phasellus tincidunt et metus quis iaculis.
 
 Nulla semper, lorem vel posuere mattis, nisl urna laoreet dolor, et ultrices justo tortor venenatis turpis. Maecenas mattis nisl sit amet lacinia luctus. In ac leo tellus. Mauris vitae elit dolor. Sed risus lacus, commodo a volutpat et, pharetra eget nulla. Fusce viverra mauris libero, ac fringilla est luctus nec. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at purus enim.
@@ -37,5 +25,17 @@ Vestibulum nibh nunc, finibus nec purus quis, fringilla feugiat ex. Praesent ves
 
 Donec lacinia aliquet placerat. Vivamus nisl leo, feugiat id eros aliquam, sagittis tempor nunc. Phasellus eget ante non justo varius ultricies at ac urna. Ut et luctus sapien. Curabitur id dictum nibh. Cras malesuada ipsum sed enim pretium ultrices. Vivamus eu luctus elit. Etiam mauris nulla, egestas at ultrices cursus, tincidunt id arcu. Duis blandit viverra purus non ornare. Ut rhoncus ornare dignissim. Cras bibendum placerat lectus, et lacinia magna lobortis semper. Fusce vitae convallis nisl. Nam fermentum lacus eget sem fringilla, vitae rhoncus eros dictum.</h1>
     </div>
+    <Card>
+        Card lul<br>
+        Card lul<br>
+        Card lul<br>
+        Card lul<br>
+      </Card>
+      <Card>
+        Card lul<br>
+        Card lul<br>
+        Card lul<br>
+        Card lul<br>
+      </Card>
   </main>
 </template>
